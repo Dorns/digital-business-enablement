70466a32b287b185ce84f5c4a0dbc817c4d48ec9
logonrm <logonrm@fiap.com.br>
2017-08-17 22:39:06 -0300

    Foto

--- 01-JSF-Primefaces/src/br/com/fiap/bean/ClienteBean.java
+++ 01-JSF-Primefaces/src/br/com/fiap/bean/ClienteBean.java
@@ -1,22 +1,33 @@
 package br.com.fiap.bean;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Calendar;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import br.com.fiap.bo.ClienteBO;
 import br.com.fiap.entity.Cliente;
 import br.com.fiap.exception.DBException;
 
 @ManagedBean
+@SessionScoped
 public class ClienteBean {
 
 	private Cliente cliente;
 	
 	private ClienteBO bo;
+	
+	private StreamedContent foto;
 	
 	@PostConstruct
 	private void init(){
@@ -52,4 +63,34 @@ public class ClienteBean {
 		this.cliente = cliente;
 	}
 	
+	public void uploadFile(FileUploadEvent event){
+		//Recuperar o nome do arquivo
+		String arquivo = event.getFile().getFileName();
+		try {
+			//Criar o arquivo no servidor
+			File file = new File("C:\\fotos\\",arquivo);
+			//Gravar o conteúdo no arquivo
+			FileOutputStream stream = new FileOutputStream(file);
+			stream.write(event.getFile().getContents());
+			stream.close();
+			cliente.setFoto(arquivo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public StreamedContent getFoto(){
+		DefaultStreamedContent content = new DefaultStreamedContent();
+		content.setContentType("image/jpg");
+		try{
+			if(FacesContext.getCurrentInstance().getRenderResponse() || cliente.getFoto() == null){
+				content.setStream(new FileInputStream("C:\\fotos\\sem-foto.jpg"));
+			}else{
+				content.setStream(new FileInputStream("C:\\fotos\\" + cliente.getFoto()));
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return content;
+	}
 }
--- 01-JSF-Primefaces/src/br/com/fiap/bean/GraficoBean.java
+++ 01-JSF-Primefaces/src/br/com/fiap/bean/GraficoBean.java
@@ -3,6 +3,10 @@ package br.com.fiap.bean;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
 import org.primefaces.model.chart.PieChartModel;
 
 import br.com.fiap.bo.ClienteBO;
@@ -11,6 +15,7 @@ import br.com.fiap.bo.ClienteBO;
 public class GraficoBean {
 
 	private PieChartModel pizza;
+	private LineChartModel linha;
 	private ClienteBO bo;
 	
 	@PostConstruct
@@ -24,6 +29,18 @@ public class GraficoBean {
 						+ bo.contarPorRating(2));
 		pizza.setShowDataLabels(true);
 		pizza.setLegendPosition("e");
+		
+		//Carregar as informações do grafico de linha
+		linha = new LineChartModel();
+		linha.setLegendPosition("e");
+		linha.getAxes().put(AxisType.X, new CategoryAxis("Mes"));
+		
+		LineChartSeries serie = new LineChartSeries();
+		serie.setLabel("Cliente");
+		for (int i=1; i<=12; i++){
+			serie.set(i, bo.contarPorMes(i));
+		}
+		linha.addSeries(serie);
 	}
 
 	public PieChartModel getPizza() {
@@ -33,5 +50,12 @@ public class GraficoBean {
 	public void setPizza(PieChartModel pizza) {
 		this.pizza = pizza;
 	}
-	
+
+	public LineChartModel getLinha() {
+		return linha;
+	}
+
+	public void setLinha(LineChartModel linha) {
+		this.linha = linha;
+	}
 }
